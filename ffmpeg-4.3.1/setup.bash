7f85e50b77ebcbb2dab84f3bc26f73f299c85069
WS01Dev <wsdev@null.dev>
2021-01-06 19:53:13 +0700

    first!

---
+++ ffmpeg-4.3.1/setup.bash
@@ -0,0 +1,3 @@
+./configure --enable-libfdk-aac --enable-gpl --enable-nonfree --enable-libx264 --build-suffix=-shvideo --enable-libvorbis --enable-libopus --enable-libvpx --enable-libmp3lame
+make
+
